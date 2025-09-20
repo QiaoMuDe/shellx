c3f6aa0f7f3272e2aab1c8a20220fc2718eb5632
M乔木 <2776617348@qq.com>
2025-09-20 12:20:45 +0800

    feat(Command): 为Command结构体添加执行相关方法
    新增了Cmd、Exec、ExecOutput、ExecAsync和Wait方法，用于支持命令的执行和异步操作。

--- Command.go
+++ Command.go
@@ -6,3 +6,23 @@ import "os/exec"
 type Command struct {
 	cmd *exec.Cmd
 }
+
+func (c *Command) Cmd() *exec.Cmd {
+	return c.cmd
+}
+
+func (c *Command) Exec() error {
+	return nil
+}
+
+func (c *Command) ExecOutput() ([]byte, error) {
+	return nil, nil
+}
+
+func (c *Command) ExecAsync() error {
+	return nil
+}
+
+func (c *Command) Wait() error {
+	return nil
+}
